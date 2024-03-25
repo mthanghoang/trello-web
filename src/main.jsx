d4e2a8b26b64c633ddb395b0c51bce58a9159d2c
Hoang Minh Thang <mthang.hoang739@gmail.com>
2024-03-25 22:25:44 +0300

    dark_light_p1

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
+// import { ThemeProvider } from '@mui/material/styles'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+
 import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       <CssBaseline>
         <App />
       </CssBaseline>
-    </ThemeProvider>
+    </CssVarsProvider>
   </React.StrictMode>
 )

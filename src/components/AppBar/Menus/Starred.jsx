1cf996b55062ce5a9237c78c963e45274600be0f
Hoang Minh Thang <mthang.hoang739@gmail.com>
2024-03-26 10:33:35 +0300

    App bar p1

---
+++ src/components/AppBar/Menus/Starred.jsx
@@ -0,0 +1,48 @@
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Box from '@mui/system/Box'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+import React from 'react'
+
+function Starred() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <Box>
+      <Button
+        id="basic-button-starred"
+        aria-controls={open ? 'basic-menu-starred' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon={<ExpandMoreIcon />}
+      >
+        starred
+      </Button>
+      <Menu
+        id="basic-menu-starred"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-starred'
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Starred
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,16 +1,76 @@
 import ModeSelect from '~/components/ModeSelect'
 import Box from '@mui/system/Box'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Workspaces from './Menus/Workspaces'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import Profile from './Menus/Profile'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Badge from '@mui/material/Badge'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 
 function AppBar() {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.light',
+    <Box px={2} sx={{
       width: '100%',
       height: (theme) => theme.custom.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon
+            component={TrelloIcon}
+            inheritViewBox
+            sx={{ color: 'primary.main' }} />
+          <Typography
+            variant='span'
+            sx={{
+              fontSize: '1.2rem',
+              fontWeight: '500',
+              color: 'primary.main'
+            }}>
+            Trello</Typography>
+        </Box>
+        <Workspaces />
+        <Recent />
+        <Starred />
+        <Templates />
+
+        <Button variant='outlined'>Create</Button>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 2
+      }}>
+        <TextField
+          id="outlined-search"
+          label="Search"
+          variant="outlined"
+          size='small'
+          type='search' />
+        <ModeSelect />
+        <Tooltip title="Notifications">
+          <Badge sx={{ cursor: 'pointer' }} color="secondary" variant="dot">
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title='Help'>
+          <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+        </Tooltip>
+        <Profile />
+      </Box>
     </Box>
   )
 }

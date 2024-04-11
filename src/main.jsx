ac096ab7f39a5dffaf4159389b87255c983743bb
Hoang Minh Thang <mthang.hoang739@gmail.com>
2024-04-11 23:11:19 +0300

    custom dndkit sensorsto prevent some elements from being draggable

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -10,12 +9,17 @@ import theme from '~/theme.js'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+// Mui Confirm Dialog library
+import { ConfirmProvider } from 'material-ui-confirm'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
+  // <React.StrictMode>
+  <CssVarsProvider theme={theme}>
+    <ConfirmProvider>
       <CssBaseline />
       <App />
       <ToastContainer position='top-left' autoClose={1000}/>
-    </CssVarsProvider>
-  </React.StrictMode>
+    </ConfirmProvider>
+  </CssVarsProvider>
+  // </React.StrictMode>
 )
